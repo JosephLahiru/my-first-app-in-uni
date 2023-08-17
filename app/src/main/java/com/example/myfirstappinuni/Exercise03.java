e78ba0f670ff3cd99ab6a97b7a20047cc75adef1
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-08-17 17:13:16 +0530

    Added Missing Project Files

---
+++ app/src/main/java/com/example/myfirstappinuni/Exercise03.java
@@ -0,0 +1,37 @@
+package com.example.myfirstappinuni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Exercise03 extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button;
+    private boolean isTextOne = true; // Initial state
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise03);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isTextOne) {
+                    textView.setText("Welcome to Android Studio!!!");
+                } else {
+                    textView.setText("Hello, World!");
+                }
+
+                isTextOne = !isTextOne;
+            }
+        });
+    }
+}
